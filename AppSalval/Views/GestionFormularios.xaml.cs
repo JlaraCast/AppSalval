76a9fe47bb39188a211d90f6e94b8a18deaf5c6a
sehjo <sehjo123@gmail.com>
2025-02-13 14:54:42 -0600

    se actualizo Appsalval

---
+++ AppSalval/Views/GestionFormularios.xaml.cs
@@ -0,0 +1,24 @@
+namespace AppSalval.Views;
+using AppSalval.Controllers;
+public partial class GestionFormularios : ContentPage
+{
+    //private GestionFormulariosController _controller;
+	public GestionFormularios()
+	{
+		InitializeComponent();
+      //  _controller = new GestionFormulariosController();
+
+    }
+
+    /*
+    private void OnOpcionesClicked(object sender, EventArgs e)
+    {
+        _controller.HandleOpcionesClicked();
+    }
+
+    private void OnMenuPrincipalClicked(object sender, EventArgs e)
+    {
+        _controller.HandleMenuPrincipalClicked();
+    }
+    */
+}
