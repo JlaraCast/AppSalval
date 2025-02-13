915f8b71029e9a9cb4768e4bbd3b52a1443d4201
sehjo <sehjo123@gmail.com>
2025-02-12 23:04:23 -0600

    se subio la ventana de inicio Admin

--- AppSalval/obj/Debug/net9.0-android/AppSalval.AssemblyInfo.cs
+++ AppSalval/obj/Debug/net9.0-android/AppSalval.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b3744d7035335e64ba7c87b220ce8cdc60bb4899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+cf80b5e26ace82335976bd662ab65cb9a51bd1d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AppSalval/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/AppSalval.AssemblyInfo.cs
+++ AppSalval/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/AppSalval.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b3744d7035335e64ba7c87b220ce8cdc60bb4899")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+14ef3a27daf615baf1def81965eb9ceef156a77c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
