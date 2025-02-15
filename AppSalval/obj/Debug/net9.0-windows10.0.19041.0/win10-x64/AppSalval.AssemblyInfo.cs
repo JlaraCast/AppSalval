b5c1197f9b8fa4acede8ec4bef86efe8344dfbe7
Kev <ualvaradokevin2005@gmail.com>
2025-02-14 22:37:38 -0600

    Se actualiza la conexión y se muestrean los formularios que solamente están habilitados

--- AppSalval/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/AppSalval.AssemblyInfo.cs
+++ AppSalval/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/AppSalval.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+41c0f02b11906881b6d4ba72cd7671e89335798d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+25fae584a1a43269cf379ba252941b16d4504820")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppSalval")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
