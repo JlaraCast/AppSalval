9764e413019e35ac5bb8f83bbc5ac16e238b008e
sehjo <sehjo123@gmail.com>
2025-02-17 15:32:35 -0600

    Squashed commit of the following:
    commit 23c634d2609f4f6c09343939ba73737cce6f1fc3
    Author: sehjo <sehjo123@gmail.com>
    Date:   Mon Feb 17 15:31:20 2025 -0600
    
        se modifico gestion Formularios para siga la estrutura del modelo MVVM
    
    commit 31c3e686d4681ab6b6e2c08da07e80485b296cc9
    Author: sehjo <sehjo123@gmail.com>
    Date:   Mon Feb 17 14:02:30 2025 -0600
    
        se agrego al viewModel de creacionFormularios
    
    commit d8f5fe8f6a5abc392ca26e94984cd94c54d8f1f3
    Author: sehjo <sehjo123@gmail.com>
    Date:   Sun Feb 16 15:15:29 2025 -0600
    
        agregaron Botones Inciar y cancelar

---
+++ AppSalval/ViewModels/CrearFormularioViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AppSalval.DTOS_API;
+using AppSalval.Services;
+using AppSalval.Views;
+
+namespace AppSalval.ViewModels
+{
+    public partial class CrearFormularioViewModel : BaseViewModel
+    {
+        private readonly INavigation _navigation;
+        private string _titulo;
+        private string _descripcion;
+        private DateTime _fechaInicio;
+        private DateTime _fechaFin;
+        private bool _requiereDatosPersonales;
+        private bool _habilitado;
+
+        public ICommand BtnCancelar { get; }
+        public ICommand BtnAceptar{ get; }
+
+        public string Titulo
+        {
+            get => _titulo;
+            set => SetProperty(ref _titulo, value);
+        }
+        public string Descripcion
+        {
+            get => _descripcion;
+            set => SetProperty(ref _descripcion, value);
+        }
+
+        public DateTime FechaInicio
+        {
+            get => _fechaInicio;
+            set => SetProperty(ref _fechaInicio, value);
+        }
+
+        public DateTime FechaFin
+        {
+            get => _fechaFin;
+            set => SetProperty(ref _fechaFin, value);
+        }
+
+        public bool Habilitado
+        {
+            get => _habilitado;
+            set => SetProperty(ref _habilitado, value);
+        }
+
+        public bool RequiereDatosPersonales
+        {
+            get => _requiereDatosPersonales;
+            set => SetProperty(ref _requiereDatosPersonales, value);
+        }
+
+
+
+        public CrearFormularioViewModel(INavigation navigation)
+        {
+            _navigation = navigation;
+            _titulo = string.Empty;
+            _descripcion = string.Empty;
+            _fechaInicio = DateTime.Now;
+            _fechaFin = DateTime.Now;
+            _requiereDatosPersonales = false;
+            _habilitado = true;
+
+            
+            BtnCancelar = new Command(async () =>
+            {
+                await _navigation.PushAsync(new GestionFormularios());
+            });
+
+            /* ingresar la funcionalidad de la creacion de un formulario
+            BtnCancelar = new Command(async () =>
+            {
+                await _navigation.PushAsync(new GestionFormularios());
+            });
+
+            */
+
+        }// Fin del constructor
+
+
+    }//fin de la clase
+}//Fin del Namespace
