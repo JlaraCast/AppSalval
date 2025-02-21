f57e5edaf1333530624c90fab46358ff98414ab3
JoanR67 <joanmx794@gmail.com>
2025-02-21 13:03:21 -0600

    Navegación InicioDesarrollador
    Se agregó la navegación para la vista inicioDesarrollador

--- AppSalval/ViewModels/InicioDesarrolladorViewModel.cs
+++ AppSalval/ViewModels/InicioDesarrolladorViewModel.cs
@@ -7,16 +7,20 @@ using System.Windows.Input;
 
 namespace AppSalval.ViewModels
 {
-    
+
     public class InicioDesarrolladorViewModel : BindableObject
     {
         public ICommand NavigateToRecomPageCommand { get; }
         public ICommand NavigateToRiskPageCommand { get; }
+        public ICommand NavigateToFormulariosPageCommand { get; }
+        public ICommand NavigateToPreguntasPageCommand { get; }
 
         public InicioDesarrolladorViewModel(INavigation navigation)
         {
             NavigateToRecomPageCommand = new Command(async () => await navigation.PushAsync(new Views.RecomPage()));
             NavigateToRiskPageCommand = new Command(async () => await navigation.PushAsync(new Views.RiskPage()));
+            NavigateToFormulariosPageCommand = new Command(async () => await navigation.PushAsync(new Views.GestionFormularios()));
+            NavigateToPreguntasPageCommand = new Command(async () => await navigation.PushAsync(new Views.GestionPreguntas()));
         }
     }
 }
--- AppSalval/Views/InicioEncuestador.xaml.cs
+++ AppSalval/Views/InicioEncuestador.xaml.cs
@@ -3,20 +3,23 @@ using System;
 
 namespace AppSalval.Views
 {
-    public InicioEncuestador()
+    public partial class InicioEncuestador : ContentPage
     {
-        InitializeComponent();
-    }
+        public InicioEncuestador()
+        {
+            InitializeComponent();
+        }
 
-    private async void OnBackButtonClicked(object sender, EventArgs e)
-    {
-        // Navega a la p�gina de login
-        await Navigation.PushAsync(new LoginPage());
-    }
+        private async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            // Navega a la página de login
+            await Navigation.PushAsync(new LoginPage());
+        }
 
-    private async void OnFormularioClicked(object sender, EventArgs e)
-    {
-        // Navega a la p�gina de encuestas
-        await Navigation.PushAsync(new Formularios());
+        private async void OnFormularioClicked(object sender, EventArgs e)
+        {
+            // Navega a la página de encuestas
+            await Navigation.PushAsync(new Formularios());
+        }
     }
 }
