ff4943b6932e77c8d99587e2934c30d6645c8cd7
JoanR67 <joanmx794@gmail.com>
2025-02-20 23:17:17 -0600

    Navegación, login y cambios de diseño
    Se estableció la navegación de la app para las vistas inicioEncuestador e inicoAdmin. Ahora el login es la primera vista que se muestra al ejecutar la app. Se hicieron cambios de diseño en las vistas correspondientes al modulo usuarios acordes a la paleta de colores acordada.

--- AppSalval/Views/InicioEncuestador.xaml.cs
+++ AppSalval/Views/InicioEncuestador.xaml.cs
@@ -2,8 +2,20 @@ namespace AppSalval.Views;
 
 public partial class InicioEncuestador : ContentPage
 {
-	public InicioEncuestador()
-	{
-		InitializeComponent();
-	}
+    public InicioEncuestador()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        // Navega a la página de login
+        await Navigation.PushAsync(new LoginPage());
+    }
+
+    private async void OnFormularioClicked(object sender, EventArgs e)
+    {
+        // Navega a la página de encuestas
+        await Navigation.PushAsync(new Formularios());
+    }
 }
